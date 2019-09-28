82a6acde3a30837a89f056ef51b4928510f81eeb
mcieslak94 <meikecieslak94@gmail.com>
2019-09-29 01:42:49 +0200

    Update BenutzerController.java

--- src/main/java/de/dohack/dolphins/controller/BenutzerController.java
+++ src/main/java/de/dohack/dolphins/controller/BenutzerController.java
@@ -1,5 +1,6 @@
 package de.dohack.dolphins.controller;
 
+import de.dohack.dolphins.models.Benutzer;
 import de.dohack.dolphins.repo.BenutzerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,9 +14,47 @@ public class BenutzerController {
 
   @Autowired BenutzerRepository repository;
 
-  @PostMapping("/benutzer/erstellen")
-  public String create(@RequestBody String string) {
-    System.out.println(string);
-    return "Benutzer wurde erstellt";
+  @RequestMapping("/benutzer/finde/{drucksachenNr}")
+  public String search(@PathVariable String drucksachenNr) {
+    String benutzer = "";
+    benutzer = repository.findById(drucksachenNr).toString();
+    return benutzer;
+  }
+
+  @RequestMapping(
+      value = "/benutzer/erstellen",
+      method = RequestMethod.POST,
+      consumes = {"multipart/form-data"})
+  public String create(
+      @RequestParam("File") MultipartFile multipartFile,
+      @RequestParam("Dokumenttitel") String titel,
+      @RequestParam("Realname") String realname,
+      @RequestParam("Anzeigename") String anzeigename,
+      @RequestParam("Email") String email,
+      @RequestParam("Passwort") String passwort, 
+      @RequestParam("Administrator") String adminstrator){
+    String dateipfad = localPath + titel.trim();
+    Benutzer benutzer = new Benutzer();
+    benutzer.setPasswort(passwort);
+    benutzer.setAnzeigename(anzeigename);
+    benutzer.setEmail(email);
+    benutzer.setAdministrator(adminstrator);
+    benutzer.setRealname(realname);
+    try {
+      multipartFile.transferTo(Paths.get(dateipfad));
+      benutzer.setDateipfad(dateipfad);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    benutzer = repository.save(benutzer);
+
+    // TODO Tags hinzufügen
+
+    return "Benutzer " + benutzer.getDrucksachennr() + " wurde erstellt";
+  }
+
+  @GetMapping("/benutzer/hallo")
+  public String benutzer() {
+    return "Hallo";
   }
 }
